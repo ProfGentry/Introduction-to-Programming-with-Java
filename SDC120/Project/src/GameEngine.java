b436eb493e3e2e59b858a25a9dfb0a27a60ac9a5
Jennifer Gentry <csprofgentry@gmail.com>
2026-01-28 13:42:40 -0500

    Update GameEngine.java

--- SDC120/Project/src/GameEngine.java
+++ SDC120/Project/src/GameEngine.java
@@ -1,1 +1,18 @@
-/** Week 1 - Game Engine Skeleton Template */
+/*************************************************************
+// COURSE WEEK ALIGNMENT
+//
+// Week 1:
+// - Program entry point
+// - Console output
+// - Basic program structure
+//
+// Week 2:
+// - Menu-driven program startup
+// - Delegating control to another class (GameEngine)
+//
+// NOTE:
+// This file should ONLY start the program.
+// All game logic belongs elsewhere.
+
+
+
