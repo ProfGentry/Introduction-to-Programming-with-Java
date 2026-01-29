c6c989afc3b74b573d4653aa6f80097b364dcfd3
Jennifer Gentry <csprofgentry@gmail.com>
2026-01-29 09:43:41 -0500

    Create Broken.java

---
+++ SDC120L/Broken.java
@@ -0,0 +1,39 @@
+import java.util.Scanner
+
+public class BrokenMenuApp {
+
+    public static void main(String args[]) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Welcome to the Menu Program")
+        System.out.println("1. Say Hello");
+        System.out.println("2. Say Goodbye");
+        System.out.println("3. Exit");
+
+        System.out.print("Enter your choice: ");
+        int choice = input.nextInt();
+
+        while (choice != 3) {
+
+            if (choice = 1) {
+                System.out.println("Hello there!");
+            }
+            else if (choice == 2) {
+                System.out.println("Goodbye!");
+            }
+            else {
+                System.out.println("Invalid option");
+            }
+
+            System.out.println("1. Say Hello");
+            System.out.println("2. Say Goodbye");
+            System.out.println("3. Exit");
+
+            System.out.print("Enter your choice: ");
+            choice = input.nextInt();
+        }
+
+        System.out.println("Program ended. Goodbye!");
+    }
+}
